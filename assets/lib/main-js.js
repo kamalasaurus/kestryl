5842ab5bf07387ddafc73e3d7e56c476fe75f334
kamalasaurus <kamalasaurus@gmail.com>
2015-10-08 12:15:49 -0700

    switching to more canonical mithril

--- assets/lib/main-js.js
+++ assets/lib/main-js.js
@@ -5,12 +5,10 @@ module.exports = {
   file: multiline.stripIndent(function(){/*
     import m from 'mithril';
 
-    import Index from './index';
     import ExampleComponent from './example-component';
 
     m.route(document.body, '/', {
-      '/': new Index(),
-      '/component/:componentId': new ExampleComponent()
+      '/': ExampleComponent
     });
 
     */})
