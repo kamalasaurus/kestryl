815b4459ca1172782741693a8bcc3b4126c1a22d
kamalasaurus <kamalasaurus@gmail.com>
2015-12-04 00:52:31 -0500

    should generate viable .babelrc

---
+++ babelrc/babelrc-react.js
@@ -0,0 +1,11 @@
+var multiline = require('multiline');
+
+module.exports = {
+  filename: '.babelrc',
+  file: multiline.stripIndent(function(){/*
+    {
+      "presets": ["es2015", "stage-0", "react"]
+    }
+  */})
+}
+
--- scripts/init.js
+++ scripts/init.js
@@ -4,16 +4,22 @@ module.exports = function(options) {
     DEPENDENCIES
   ================================================================== */
 
-  var close    = require('../functions/close');
-  var exe      = require('../functions/exe');   // run code synchronously
+  var close     = require('../functions/close');
+  var exe       = require('../functions/exe');   // run code synchronously
 
-  var addGit   = require('../scripts/init-1');  // add a git repo after initializing init
+  var addGit    = require('../scripts/init-1');  // add a git repo after initializing init
 
-  var content  = exe('ls -A | wc -l', true);    // checks if target directory has contents
+  var content   = exe('ls -A | wc -l', true);    // checks if target directory has contents
 
-  var babelrc  = options.withReact ?
-                 JSON.stringify(require('../lib/babelrc-react')) :
-                 JSON.stringify(require('../lib/babelrc-mithril'));
+  var babelrc   = options.withReact ?
+                 require('../babelrc/babelrc-react') :
+                 require('../babelrc/babelrc-mithril');
+
+  var dirArray  = exe('pwd', true).split('/');
+                 dirArray.splice('-1','1','babelrc');
+  var babelDir  = dirArray.join('/');
+
+  var writeFile = require('../functions/write-file');
 
   /* ==================================================================
     INITIALIZATION SCRIPT
@@ -27,13 +33,17 @@ module.exports = function(options) {
   // create kestryl config file
   // TODO: actually add the config list (react vs. mithril primarily)
   exe('touch .kestryl');
-  exe('touch .babelrc');
 
   if (options.withReact) {
     exe('echo "react" >> .kestryl');
   } else {
     exe('echo "mithril" >> .kestryl');
   }
+
+  // create babelrc
+  writeFile(babelDir, babelrc.filename, babelrc.file);
+
+
   exe('echo ' + babelrc + ' >> .babelrc');
   // git init
   exe('git init');
