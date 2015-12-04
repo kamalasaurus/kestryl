4eb64e32baadf6cd62c02186df161f859aac0cac
kamalasaurus <kamalasaurus@gmail.com>
2015-12-04 00:59:21 -0500

    trying again

--- scripts/init.js
+++ scripts/init.js
@@ -12,14 +12,8 @@ module.exports = function(options) {
   var content   = exe('ls -A | wc -l', true);    // checks if target directory has contents
 
   var babelrc   = options.withReact ?
-                 require('../babelrc/babelrc-react') :
-                 require('../babelrc/babelrc-mithril');
-
-  var dirArray  = exe('pwd', true).split('/');
-                 dirArray.splice('-1','1','babelrc');
-  var babelDir  = dirArray.join('/');
-
-  var writeFile = require('../functions/write-file');
+                 "{ \"presets\": [\"es2015\", \"stage-0\", \"react\"] }" :
+                 "{ \"presets\": [\"es2015\", \"stage-0\"] }";
 
   /* ==================================================================
     INITIALIZATION SCRIPT
@@ -33,6 +27,7 @@ module.exports = function(options) {
   // create kestryl config file
   // TODO: actually add the config list (react vs. mithril primarily)
   exe('touch .kestryl');
+  exe('touch .babelrc');
 
   if (options.withReact) {
     exe('echo "react" >> .kestryl');
@@ -41,10 +36,8 @@ module.exports = function(options) {
   }
 
   // create babelrc
-  writeFile(babelDir, babelrc.filename, babelrc.file);
-
-
   exe('echo ' + babelrc + ' >> .babelrc');
+
   // git init
   exe('git init');
 
